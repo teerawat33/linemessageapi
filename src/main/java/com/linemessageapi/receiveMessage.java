7ad979b17d02cbed3356210489742eef502f4c52
teerawat33 <teerawat@antinode.co.th>
2019-02-06 15:02:58 +0700

    update v.4

--- src/main/java/com/linemessageapi/receiveMessage.java
+++ src/main/java/com/linemessageapi/receiveMessage.java
@@ -1,19 +1,99 @@
 package com.linemessageapi;
 
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @LineMessageHandler
 public class receiveMessage {
+    @Autowired
+    private LineMessagingClient lineMessagingClient;
 
     @EventMapping
     public void handleTextMessage(MessageEvent<TextMessageContent> event) {
 
         //TODO ส่งไปไหน
         TextMessageContent message = event.getMessage();
+
         String userId = event.getSource().getUserId();
         String setMessage = message.getText();
+
+        lineMessagingClient.getProfile(userId).whenComplete(((userProfileResponse, throwable) -> {
+
+            handleTextContent(event.getReplyToken(), event, message);
+        }));
+
+    }
+
+    private void handleTextContent(String replyToken, Event event,
+                                   TextMessageContent content) {
+        String text = content.getText();
+
+        switch (text) {
+            case "test": {
+                String userId = event.getSource().getUserId();
+                if (userId != null) {
+                    lineMessagingClient.getProfile(userId)
+                            .whenComplete((profile, throwable) -> {
+                                if (throwable != null) {
+                                    this.replyText(replyToken, throwable.getMessage());
+                                    return;
+                                }
+                                this.reply(replyToken, Arrays.asList(
+                                        new TextMessage("Display name: " +
+                                                profile.getDisplayName()),
+                                        new TextMessage("Status message: " +
+                                                profile.getStatusMessage()),
+                                        new TextMessage("User ID: " +
+                                                profile.getUserId()),
+                                        new TextMessage("Token: " +
+                                                replyToken)
+
+                                ));
+                            });
+                }
+                break;
+            }
+            default:
+                this.replyText(replyToken, text);
+        }
+    }
+
+    private void replyText(String replyToken, String message) {
+        if (replyToken.isEmpty()) {
+            throw new IllegalArgumentException("replyToken is not empty");
+        }
+
+        if (message.length() > 1000) {
+            message = message.substring(0, 1000 - 2) + "...";
+        }
+        this.reply(replyToken, new TextMessage(message));
+    }
+
+    private void reply(String replyToken, Message message) {
+        reply(replyToken, Collections.singletonList(message));
+    }
+
+    private void reply(String replyToken, List<Message> messages) {
+        try {
+            BotApiResponse response = lineMessagingClient.replyMessage(
+                    new ReplyMessage(replyToken, messages)
+            ).get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
