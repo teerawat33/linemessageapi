4b9ef8895dad6345468efcd549a1e71291c630b9
teerawat33 <teerawat@antinode.co.th>
2019-02-06 10:57:12 +0700

    start

---
+++ src/main/java/com/linemessageapi/LinemessageapiApplication.java
@@ -0,0 +1,14 @@
+package com.linemessageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinemessageapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinemessageapiApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/linemessageapi/LinemessageapiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.linemessageapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LinemessageapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
